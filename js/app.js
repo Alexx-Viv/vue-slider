c10a1957a04080fe8017150b4a99f414c6f1da31
Alexx-Viv <mario.alex.vivsenco@gmail.com>
2021-08-30 16:35:00 +0200

    Finish(?)

---
+++ js/app.js
@@ -0,0 +1,33 @@
+Vue.config.devtools = true;
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    currentIndex: 0,
+    images: [
+      'images/image1.jpg',
+      'images/image2.jpg',
+      'images/image3.jpg',
+      'images/image4.jpg',
+    ],
+  },
+  methods: {
+    isActive(index) {
+      return this.currentIndex === index ? 'active' : '';
+    },
+    increaseIndex() {
+      if (this.currentIndex === this.images.length - 1) {
+        this.currentIndex = 0;
+      } else {
+        this.currentIndex++;
+      }
+    },
+    decreaseIndex() {
+      if (this.currentIndex === 0) {
+        this.currentIndex = this.images.length - 1;
+      } else {
+        this.currentIndex--;
+      }
+    },
+  },
+});
